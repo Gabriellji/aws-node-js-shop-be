61011740a62bb89380c5c670fb7023b8497f6d3f
Gabriellji <gabriellji@yahoo.com>
2021-08-29 21:40:08 +0400

    fix: add headers to response

--- shop-service/src/error-handler/api_responses.ts
+++ shop-service/src/error-handler/api_responses.ts
@@ -1,26 +1,42 @@
 export const apiResponses = {
-    _200: (body: { [key: string]: any }) => {
-        return {
-            statusCode: 200,
-            body: JSON.stringify(body, null, 2),
-        };
-    },
-    _404: (body: { [key: string]: any }) => {
-        return {
-            statusCode: 404,
-            body: JSON.stringify(body, null, 2),
-        };
-    },
-    _400: (body: { [key: string]: any }) => {
-        return {
-            statusCode: 400,
-            body: JSON.stringify(body, null, 2),
-        };
-    },
-    _500: (body: { [key: string]: any }) => {
-        return {
-            statusCode: 500,
-            body: JSON.stringify(body, null, 2),
-        };
-    },
+  _200: (body: { [key: string]: any }) => {
+    return {
+      statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Credentials": true,
+      },
+      body: JSON.stringify(body, null, 2),
+    };
+  },
+  _404: (body: { [key: string]: any }) => {
+    return {
+      statusCode: 404,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Credentials": true,
+      },
+      body: JSON.stringify(body, null, 2),
+    };
+  },
+  _400: (body: { [key: string]: any }) => {
+    return {
+      statusCode: 400,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Credentials": true,
+      },
+      body: JSON.stringify(body, null, 2),
+    };
+  },
+  _500: (body: { [key: string]: any }) => {
+    return {
+      statusCode: 500,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Credentials": true,
+      },
+      body: JSON.stringify(body, null, 2),
+    };
+  },
 };
